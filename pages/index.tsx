cc4f2a8c147e29a10fc24539e5ad075d0af039d2
Nikhil <nikhil@Nikhils-MacBook-Air.local>
2022-04-30 21:37:23 +0530

    added

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,8 +4,9 @@ import Image from 'next/image'
 import  Header from '../components/Header'
 import  Feed from '../components/Feed'
 import Modal from '../components/Modal'
+import { signIn, signOut, useSession } from 'next-auth/react'
 
-
+const { data: session } = useSession()
 const Home: NextPage = () => {
   
   
@@ -23,6 +24,17 @@ const Home: NextPage = () => {
       <Header />
 
       <Feed />
+
+      {!session && (
+        <div className="flex flex-col items-center justify-center min-h-screen py-2 -mt-56 px-14 text-center ">
+        <img className="w-80" src="https://links.papareact.com/ocw"
+        alt="" />
+        <p className="font-xs italic">
+          This is not real app this for portfolio purpose
+        </p>
+        </div>
+
+      )}
       
       <Modal />
     </div>
